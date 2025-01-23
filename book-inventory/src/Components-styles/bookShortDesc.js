0bfce3ac437259b784e004b99d8b076055c5fc03
jmilind1234 <jmilind1234@gmail.com>
2025-01-23 16:05:13 +0530

    created styled sub-components for Book description.

---
+++ book-inventory/src/Components-styles/bookShortDesc.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const BookDescription = styled.div`
+    border: 1px solid black;
+    border-radius: 5px;
+    background-color: rgb(245, 179, 179);
+    margin-top:30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const BookTitle = styled.div`
+    border: 1px solid blue;
+    width: 30%;
+    height: 50px;
+`;
+
+export const ButtonHolder = styled.div`
+    border: 1px solid red;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
