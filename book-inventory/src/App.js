2db3c071209372a721920322d78bf2a88b01b51e
jmilind1234 <jmilind1234@gmail.com>
2025-01-23 16:04:14 +0530

    Showing Home Component on root path.

--- book-inventory/src/App.js
+++ book-inventory/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import FeedbackForm from './Components/FeedbackForm';
 import Header from './Components/Header';
 import {Routes, Route} from "react-router-dom";
+import Home from './Components/Home';
 
 function App() {
   return (
     <>
       <Header/>
       <Routes>
-        <Route path="/" element={<></>}></Route>
+        <Route path="/" element={<><Home/></>}></Route>
         <Route path="about" element={<h1>Dummy about page</h1>}></Route>
         <Route path="contact" element={<h1>Dummy contact page</h1>}></Route>
         <Route path="feedback" element={<><FeedbackForm/></>}></Route>
