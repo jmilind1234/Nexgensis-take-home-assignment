66f010ed2fd138a389e10da4aa84a8061080e2be
jmilind1234 <jmilind1234@gmail.com>
2025-01-23 14:37:53 +0530

    aaded alert and validation to feedback form.

--- book-inventory/src/Components/FeedbackForm.js
+++ book-inventory/src/Components/FeedbackForm.js
@@ -1,5 +1,6 @@
 import { FieldHolder, FormContainer, InputField, Label, LabelHolder, RowHolder, SubmitButton } from "../Components-styles/feedbackForm";
 import React, {useState} from "react";
+import { onlyAlphabets, validateEmail } from "../utils/FormHandling";
 
 function FeedbackForm(){
     const [fieldValues, setFieldValues] = useState({
@@ -16,20 +17,64 @@ function FeedbackForm(){
         ageError:"",
     });
 
+    console.log(fieldErrors , fieldValues);
+
     const firstNameChangeHandler = (e)=>{
-        setFieldValues({...fieldValues, firstName: e?.target?.value})
+        setFieldValues({...fieldValues, firstName: e?.target?.value});
     }
 
     const lastNameChangeHandler = (e)=>{
-        setFieldValues({...fieldValues, lastName: e?.target?.value})
+        setFieldValues({...fieldValues, lastName: e?.target?.value});
     }
 
     const emailChangeHandler = (e) =>{
-        setFieldValues({...fieldValues, email: e?.target?.value});
+        setFieldValues({...fieldValues, email: e?.target?.value});  
     }
 
     const ageChangeHandler = (e) => {
         setFieldValues({...fieldValues, age: e?.target?.value});
+    }
+
+    const firstNameBlurHandler = ()=>{
+        if(fieldValues.firstName.length === 0){
+            setFieldErrors({...fieldErrors,firstNameError: "first name must not be empty."});
+        }else if(!onlyAlphabets(fieldValues.firstName)){
+            setFieldErrors({...fieldErrors,firstNameError: "first name must contain only alphabets"});
+        }
+    }
+
+    const lastNameBlurHandler = ()=>{
+        if(fieldValues.firstName.length === 0){
+            setFieldErrors({...fieldErrors,lastNameError: "last name must not be empty."});
+        }else if(!onlyAlphabets(fieldValues.lastName)){
+            setFieldErrors({...fieldErrors,lastNameError: "last name must contain only alphabets"});
+        }
+    }
+
+    const emailBlurHandler = ()=>{
+        if(fieldValues.email.length === 0){
+            setFieldErrors({...fieldErrors,emailError: "email must not be empty."});
+        }else if(!validateEmail(fieldValues.email)){
+            setFieldErrors({...fieldErrors,emailError: "email is not in proper format."});
+        }
+    }
+
+    const ageBlurHandler = ()=>{
+        if(fieldValues.age.length === 0){
+            setFieldErrors({...fieldErrors,ageError: "age must not be empty."});
+        }
+    }
+
+    const errorAlert = ()=>{
+        if(fieldErrors.firstNameError){
+            alert(fieldErrors.firstNameError);
+        }else if(fieldErrors.lastNameError){
+            alert(fieldErrors.lastNameError);
+        }else if(fieldErrors.ageError){
+            alert(fieldErrors.ageError);
+        }else if(fieldErrors.emailError){
+            alert(fieldErrors.emailError);
+        }
     }
     
     return(
@@ -40,7 +85,7 @@ function FeedbackForm(){
                     <Label for="fname">First Name</Label>
                 </LabelHolder>
                 <FieldHolder>
-                    <InputField type="text" id="fname" name="firstname" placeholder="Your name.." value={fieldValues?.firstName} onChange={firstNameChangeHandler}/>
+                    <InputField type="text" id="fname" name="firstname" placeholder="Your first name.." value={fieldValues?.firstName} onChange={firstNameChangeHandler} onBlur={firstNameBlurHandler}/>
                 </FieldHolder>
             </RowHolder>
             <RowHolder>
@@ -48,7 +93,7 @@ function FeedbackForm(){
                     <Label for="lname">Last Name</Label>
                 </LabelHolder>
                 <FieldHolder>
-                    <InputField type="text" id="lname" name="lastname" placeholder="Your last name.." value={fieldValues?.lastName} onChange={lastNameChangeHandler}/>
+                    <InputField type="text" id="lname" name="lastname" placeholder="Your last name.." value={fieldValues?.lastName} onChange={lastNameChangeHandler} onBlur={lastNameBlurHandler}/>
                 </FieldHolder>
             </RowHolder>
             <RowHolder>
@@ -56,7 +101,7 @@ function FeedbackForm(){
                     <Label for="email">Email</Label>
                 </LabelHolder>
                 <FieldHolder>
-                    <InputField type="text" id="email" name="email-address" placeholder="Enter you email address.." value={fieldValues?.email} onChange={emailChangeHandler}/>
+                    <InputField type="text" id="email" name="email-address" placeholder="Enter you email address.." value={fieldValues?.email} onChange={emailChangeHandler} onBlur={emailBlurHandler}/>
                 </FieldHolder>
             </RowHolder>
             <RowHolder>
@@ -64,12 +109,11 @@ function FeedbackForm(){
                     <Label for="age">Age</Label>
                 </LabelHolder>
                 <FieldHolder>
-                    <InputField type="number" id="age" name="Age" placeholder="Enter you age.." value={fieldValues?.age} onChange={ageChangeHandler }/>
+                    <InputField type="number" id="age" name="Age" placeholder="Enter you age.." value={fieldValues?.age} onChange={ageChangeHandler} onBlur={ageBlurHandler}/>
                 </FieldHolder>
             </RowHolder>
-        </FormContainer><SubmitButton value="SUBMIT" />
-    </>
-
+        </FormContainer><SubmitButton value="SUBMIT" onClick={errorAlert}/>
+    </> 
     )
 }
 
