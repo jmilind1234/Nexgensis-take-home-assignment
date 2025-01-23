7cf83e62908cede8bf7c1b99ac93b67d54da9258
jmilind1234 <jmilind1234@gmail.com>
2025-01-23 14:12:57 +0530

    Added FormHandling utilities.

---
+++ book-inventory/src/utils/FormHandling.js
@@ -0,0 +1,9 @@
+export const onlyAlphabets = (str) => {
+    const regex = /^[a-zA-Z]+$/;
+    return regex.test(str);
+}
+
+export const validateEmail = (email) =>{
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+}
