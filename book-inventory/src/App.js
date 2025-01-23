7db01760f6074d1d6f0a556ac22fb7a3fde13aae
jmilind1234 <jmilind1234@gmail.com>
2025-01-23 15:29:42 +0530

    created a  mock db server which will send 16 fake books details.

--- book-inventory/src/App.js
+++ book-inventory/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import FeedbackForm from './Components/FeedbackForm';
 import Header from './Components/Header';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<></>}></Route>
+        <Route path="about" element={<h1>Dummy about page</h1>}></Route>
+        <Route path="contact" element={<h1>Dummy contact page</h1>}></Route>
+        <Route path="feedback" element={<><FeedbackForm/></>}></Route>
+      </Routes>
+    </>
   );
 }
 
--- book-inventory/src/Components/Header.js
+++ book-inventory/src/Components/Header.js
@@ -1,14 +1,14 @@
 import {HeaderContainer, Logo, PagesHolder, Links} from "../Components-styles/header";
-
+import { NavLink } from "react-router-dom";
 function Header(){
     return(
         <HeaderContainer>
             <Logo>Nexgensis</Logo>
             <PagesHolder>
-                <Links href="#">Home</Links>
-                <Links href="contact">Contact</Links>
-                <Links href="about">About</Links>
-                <Links href="feedback">Feedback</Links>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="contact">Contact</NavLink>
+                <NavLink to="about">About</NavLink>
+                <NavLink to="feedback">Feedback</NavLink>
             </PagesHolder>
         </HeaderContainer>
     )
